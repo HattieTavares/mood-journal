6e6fb7779711d05f73bf970d430049624426c9c7
andiedoescode <pang.andrea@gmail.com>
2022-11-16 18:02:41 -0500

    Added model for check ins

---
+++ models/Checkin.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const CheckinSchema = new mongoose.Schema({
+    scaleNumber: {
+        type: Number,
+        required: true,
+    },
+    note: {
+        type: String,
+        required: false,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true, 
+        
+    },
+    createdById: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+});
+
+module.exports = mongoose.model("Checkin", CheckinSchema);
