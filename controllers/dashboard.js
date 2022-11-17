c08e1c3e8e2b9610e032c7baddd70562c694d556
mbcodes <mbcodes@protonmail.com>
2022-11-17 12:30:52 -0800

    updated getDashboard controller

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,14 +1,16 @@
-const moment = require('moment')
+const Checkin = require('../models/Checkin');
+// const moment = require('moment');
 
 module.exports = {
-    getIndex: (req,res)=>{
-        res.render('index.ejs')
-    },
-    getDashboard : async (req, res) => {
-        try {
-            res.render("dashboard.ejs")
-        } catch (err) {
-            if (err) return res.status(500).send(err.toString())
-        }
-    },
-}
+  getIndex: (req, res) => {
+    res.render('index.ejs');
+  },
+  getDashboard: async (req, res) => {
+    try {
+      const myCheckins = await Checkin.find({ userId: req.user.id });
+      res.render('dashboard.ejs', { checkin: myCheckins, user: req.user });
+    } catch (err) {
+      if (err) return res.status(500).send(err.toString());
+    }
+  },
+};
