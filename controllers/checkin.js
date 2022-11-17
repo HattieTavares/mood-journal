7647e5b772e9eb97afa6a6f7694720bb74a8ce44
andiedoescode <pang.andrea@gmail.com>
2022-11-17 11:23:08 -0500

    Edited controllers.

---
+++ controllers/checkin.js
@@ -0,0 +1,26 @@
+const moment = require('moment')
+const Checkin = require('../models/Checkin')
+
+module.exports = {
+
+    //GET /checkin
+    getCheckin: (req,res)=>{
+        res.render('checkin.ejs')
+    },
+
+    //POST /checkin
+    addCheckin : async (req, res) => {
+        try {
+            await Checkin.create({
+                scaleNumber: req.body.scaleNumber,
+                note: req.body.note,
+                date: req.body.date,
+                createdById:  req.user.id,
+            }),
+            console.log("Checkin has been added.");
+            res.redirect("/profile")
+        } catch (err) {
+            console.log(err);
+        }
+    },
+}
