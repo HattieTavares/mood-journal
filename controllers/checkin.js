58ee544408e821888bb34d96febb7d88afbc6284
Hattie <82660400+HattieTavares@users.noreply.github.com>
2022-11-20 16:06:05 -0500

    fixed the bug where users could check in more than once per day

--- controllers/checkin.js
+++ controllers/checkin.js
@@ -6,21 +6,33 @@ module.exports = {
     //GET /checkin
     getCheckin: (req,res)=>{
         //Retrieving today's date for default date picker
-        let today = moment().format('YYYY-MM-DD')
+        let today = moment.utc().format('YYYY-MM-DD')
         res.render('checkin.ejs', {today: today})
     },
 
     //POST /checkin
     addCheckin : async (req, res) => {
         try {
-            await Checkin.create({
-                scaleNumber: req.body.scaleNumber,
-                note: req.body.note,
-                date: req.body.date,
-                createdById:  req.user.id,
-            }),
-            console.log("Checkin has been added.");
-            res.redirect("/dashboard")
+            const myCheckins = await Checkin.find({ createdById: req.user.id }).sort({ date: "desc" }).lean();     
+            const today = moment.utc(myCheckins[0].date).format('YYYY-MM-DD')   
+            console.log(myCheckins[0], today, req.body.date)
+            if(today != req.body.date) {
+                await Checkin.create({
+                    scaleNumber: req.body.scaleNumber,
+                    note: req.body.note,
+                    date: req.body.date,
+                    createdById:  req.user.id,
+                }),
+                console.log("Checkin has been added.");
+                res.redirect("/dashboard")
+            } else {
+                const validationErrors = []
+                validationErrors.push({ msg: 'You have already checked in today.' })
+                if (validationErrors.length) {
+                    req.flash('errors', validationErrors)
+                    return res.redirect('../checkin')
+                  }
+            }
         } catch (err) {
             console.log(err);
         }
--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -9,7 +9,6 @@ module.exports = {
     try {
       const myCheckins = await Checkin.find({ createdById: req.user.id }).sort({ date: "desc" }).lean();
       res.render('dashboard.ejs', { checkin: myCheckins, user: req.user, moment:moment });
-      console.log(myCheckins)
     } catch (err) {
       if (err) return res.status(500).send(err.toString());
     }
